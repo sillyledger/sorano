431f5b1e822d7bfc2b32b2c11da2edb5c717ca10
sillyledger <tenkaro@icloud.com>
2026-06-10 17:11:21 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" style={{ background: '#1c1c24' }}>
+      <body style={{ margin: 0, padding: 0, background: '#1c1c24' }}>{children}</body>
     </html>
   )
 }
